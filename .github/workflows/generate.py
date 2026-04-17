11003bc31e761547262cccf8543c2a42888ce132
aaronwang2026 Analyst <5109343@qq.com>
2026-04-17 14:18:15 +0800

    fix: 时间格式改为 15分钟+10秒

--- .github/workflows/generate.py
+++ .github/workflows/generate.py
@@ -59,7 +59,7 @@ def get_game_list():
             if base.isdigit():
                 seconds = int(base)
                 minutes = seconds // 60 if seconds >= 60 else seconds
-                time_control = f"{minutes}'+{inc}\""
+                time_control = f"{minutes}分钟+{inc}秒"
             else:
                 time_control = time_control_raw
         elif time_control_raw.isdigit():
